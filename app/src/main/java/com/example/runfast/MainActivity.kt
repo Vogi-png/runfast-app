6e98570a44cd83b61d28fe8a1023dfa61907c7b1
Giovanna Leal <Work1now@myyahoo.com>
2026-02-23 23:45:27 -0300

    feat: fundo gradiente e statusbar com cores diferentes + ajustes

--- app/src/main/java/com/example/runfast/MainActivity.kt
+++ app/src/main/java/com/example/runfast/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.runfast.databinding.ActivityMainBinding
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
@@ -37,12 +40,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-    private fun replaceFragment (fragment : androidx.fragment.app.Fragment){
+    private fun replaceFragment(fragment: androidx.fragment.app.Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
+
+        when (fragment) {
+            is Objective -> updateStatusBarColor("#AC260F") // Laranja
+            is Profile -> updateStatusBarColor("#AC0E2B")   // Vermelho
+            is Home -> updateStatusBarColor(null)           // null para voltar ao padrão (Branco/Tema)
+            else -> updateStatusBarColor(null)
+        }
+    }
+
+    private fun updateStatusBarColor(colorHex: String?) {
+        val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
+
+        if (colorHex == null) {
+            val typedValue = android.util.TypedValue()
+            theme.resolveAttribute(android.R.attr.statusBarColor, typedValue, true)
+            window.statusBarColor = typedValue.data
+
+        } else {
+            window.statusBarColor = android.graphics.Color.parseColor(colorHex)
+
+        }
     }
 }
--- app/src/main/java/com/example/runfast/Profile.kt
+++ app/src/main/java/com/example/runfast/Profile.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
